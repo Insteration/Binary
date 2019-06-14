aa4d786d1d172c85e2081527df5a6cd74b138dc0
Artem <inste@icloud.com>
2019-06-14 19:56:26 +0300

    First commit

--- Binary/main.c
+++ Binary/main.c
@@ -8,8 +8,36 @@
 
 #include <stdio.h>
 
+void printer(void);
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int number;
+    int ostatok;
+    char result[12];
+
+    printf("Enter INT number for conversion\n");
+    scanf("%d", &number);
+    printf("Your number is - %d\n", number);
+    
+    while (number > 1) {
+        
+        ostatok = number % 2;
+        number /= 2;
+        
+        sprintf(result, "%d", ostatok);
+        printf("%s", result);
+        
+    }
+    
+    printf("%d\n", number);
+    printer();
+    
     return 0;
+}
+
+void printer() {
+    printf("\n");
+    printf("Good head! Super!\n");
+    printf("\n");
 }
