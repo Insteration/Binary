5112e86bbf958294294db0d0718742eb04cbab89
Artem <inste@icloud.com>
2019-06-14 19:07:01 +0300

    Initial Commit

---
+++ Binary/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Binary
+//
+//  Created by Artem Karmaz on 14.06.2019.
+//  Copyright © 2019 Artem Karmaz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
